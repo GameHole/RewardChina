382a9af92510091491e58637e524b689300d7318
BlackHole <38195607+GameHole@users.noreply.github.com>
2020-10-15 20:31:56 +0800

    添加动画系统

--- Helper/Animation/MoveTo.cs
+++ Helper/Animation/MoveTo.cs
@@ -1,17 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[RequireComponent(typeof(CurveRunner))]
 public class MoveTo : MonoBehaviour
 {
-    public AnimationCurve curve;
+    //public AnimationCurve curve;
     public Vector3 start;
     public Vector3 dir;
-    public float increase;
-    public float speed = 1;
+    //public float increase;
+    //public float speed = 1;
     public bool useLocal;
     public bool useCurrent;
+    CurveRunner runner;
     private void Awake()
     {
+        runner = GetComponent<CurveRunner>();
         if (useCurrent)
         {
             start = useLocal ? transform.localPosition : transform.position;
@@ -19,8 +22,8 @@ public class MoveTo : MonoBehaviour
     }
     private void Update()
     {
-        increase += Time.deltaTime * speed;
-        Vector3 outPut = start + curve.Evaluate(increase) * dir;
+        //increase += Time.deltaTime * speed;
+        Vector3 outPut = start + runner.output * dir;
         if (useLocal)
             transform.localPosition = outPut;
         else
