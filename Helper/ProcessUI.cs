427d0fa09c9b3c1e261cc8558f4bd099c9aa3ef6
BlackHole <38195607+GameHole@users.noreply.github.com>
2020-10-17 18:10:49 +0800

    通用功能调整

---
+++ Helper/ProcessUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProcessUI : MonoBehaviour
+{
+    public Image fg;
+    public Text text;
+    float _process;
+    public float process
+    {
+        get => _process;
+        set
+        {
+            _process = value;
+            if (_process > 1)
+                _process = 1;
+            fg.fillAmount = _process;
+            if (text)
+                text.text = $"{(_process * 100).ToString("f2")}%";
+        }
+    }
+}
---
+++ Helper/Watting.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using System.Threading.Tasks;
+using UnityEngine;
+public static class Waitting
+{
+    public static Task WaitAsync(float time)
+    {
+        return Task.Delay((int)(time * 1000));
+    }
+}
