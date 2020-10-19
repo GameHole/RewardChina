c32414c7118bb3bc40e302ba64bcd8698872091e
BlackHole <38195607+GameHole@users.noreply.github.com>
2020-10-19 16:15:59 +0800

    整理激励卡结构

--- Helper/FormatText.cs
+++ Helper/FormatText.cs
@@ -1,13 +1,18 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class FormatText : MonoBehaviour
 {
+    static readonly string[] empty = new string[0];
     string format;
     Text txt;
     public string text { get => !txt ? "" : txt.text; }
+    string[] tempValues = empty;
+    StringBuilder builder = new StringBuilder();
     public void SetValues(params string[] values)
     {
         if (format == null)
@@ -16,5 +21,31 @@ public class FormatText : MonoBehaviour
             format = txt.text;
         }
         txt.text = string.Format(format, values);
+    }
+    public void SetValues(int idx,string value)
+    {
+        if (format == null)
+        {
+            txt = GetComponent<Text>();
+            format = txt.text;
+        }
+        if (tempValues.Length <= idx)
+        {
+            var strs = new string[idx + 1];
+            Array.Copy(tempValues, strs, tempValues.Length);
+            tempValues = strs;
+        }
+        tempValues[idx] = value;
+        txt.text = selfFormat();
+    }
+    string selfFormat()
+    {
+        builder.Clear();
+        builder.Append(format);
+        for (int i = 0; i < tempValues.Length; i++)
+        {
+            builder.Replace("{"+i+"}", tempValues[i]);
+        }
+        return builder.ToString();
     }
 }
---
+++ Helper/UIEx.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+public static class UIEx
+{
+    public static void MakesureCapcity<T>(this List<T> items,T prefab,Transform layout, int length) where T : MonoBehaviour
+    {
+        int n = length - items.Count;
+        for (int i = 0; i < n; i++)
+        {
+            var clone = prefab.Instantiate(layout);
+            clone.transform.localScale = Vector3.one;
+            items.Add(clone);
+        }
+        for (int i = 0; i < items.Count; i++)
+        {
+            items[i].gameObject.SetActive(i < length);
+        }
+    }
+}
