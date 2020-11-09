b014f5879eebc12d22a6d56dc5b04d84146562a0
BlackHole <38195607+GameHole@users.noreply.github.com>
2020-11-09 16:58:00 +0800

    修改BUG

--- Interfaces/IMoneyForUI.cs
+++ Interfaces/IMoneyForUI.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public interface IMoneyForUI : IInterface
 {
     int conversionRatio { get; set; }
-    string ToString(int money);
+    string ToString(int money, int last = 2);
     float ToFloat(int money);
     int ToMoney(float money);
 }
